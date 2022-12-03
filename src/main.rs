4b0a4431b41ee834ff731cbbe9cb7aedfc6fa18c
IsaacDynamo <isaacdynamo@gmail.com>
2022-12-03 21:28:26 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 #![no_std]
 #![no_main]
 
-
 use esp32c3_hal as hal;
 
 use embedded_svc::wifi::{
-    ClientConfiguration, ClientConnectionStatus, ClientIpStatus, ClientStatus,
-    Configuration, Status,Wifi
+    ClientConfiguration, ClientConnectionStatus, ClientIpStatus, ClientStatus, Configuration,
+    Status, Wifi,
 };
 
 use esp_backtrace as _;
@@ -14,25 +13,22 @@ use esp_println::logger::init_logger;
 use esp_println::println;
 
 use esp_wifi::wifi::utils::create_network_interface;
-use esp_wifi::{create_network_stack_storage, network_stack_storage};
 use esp_wifi::wifi_interface::timestamp;
-
+use esp_wifi::{create_network_stack_storage, network_stack_storage};
 
 use smoltcp::socket::{TcpSocket, TcpSocketBuffer, TcpState};
 
-use hal::system::SystemExt;
 use hal::clock::{ClockControl, CpuClock};
-use hal::{pac::Peripherals, Rtc};
+use hal::system::SystemExt;
 use hal::{
-    IO,
     ledc::{
         channel::{self, ChannelIFace},
         timer::{self, TimerIFace},
-        LSGlobalClkSource,
-        LowSpeed,
-        LEDC,
+        LSGlobalClkSource, LowSpeed, LEDC,
     },
+    IO,
 };
+use hal::{pac::Peripherals, Rtc};
 
 use enumset::enum_set;
 
@@ -40,25 +36,34 @@ use esp32c3_hal::{interrupt, pac, prelude::*};
 
 use riscv_rt::entry;
 
-use esp_buffered_serial::{DataBits, Parity, StopBits, Inverse, Pins, BufferedSerial, Config};
 use esp_buffered_serial::optional_pin::UnconnectedPin;
-
-
+use esp_buffered_serial::{BufferedSerial, Config, DataBits, Inverse, Parity, Pins, StopBits};
 
 const SSID: &str = include_str!("../ssid.txt");
 const PASSWORD: &str = include_str!("../password.txt");
 
-
 fn is_starting(wifi_interface: &esp_wifi::wifi_interface::Wifi) -> bool {
-    matches!(wifi_interface.get_status(), Status(ClientStatus::Starting,_))
+    matches!(
+        wifi_interface.get_status(),
+        Status(ClientStatus::Starting, _)
+    )
 }
 
 fn is_started(wifi_interface: &esp_wifi::wifi_interface::Wifi) -> bool {
-    matches!(wifi_interface.get_status(), Status(ClientStatus::Started(_),_))
+    matches!(
+        wifi_interface.get_status(),
+        Status(ClientStatus::Started(_), _)
+    )
 }
 
 fn is_connected(wifi_interface: &esp_wifi::wifi_interface::Wifi) -> bool {
-    matches!(wifi_interface.get_status(), Status(ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(_))),_))
+    matches!(
+        wifi_interface.get_status(),
+        Status(
+            ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(_))),
+            _
+        )
+    )
 }
 
 fn esp_println_init(x: hal::pac::UART0) {
@@ -69,7 +74,7 @@ fn esp_println_init(x: hal::pac::UART0) {
 enum UplinkState {
     Normal,
     Flush,
-    Empty
+    Empty,
 }
 
 static SERIAL: BufferedSerial<hal::pac::UART1, 512, 64> = BufferedSerial::new();
@@ -98,11 +103,14 @@ fn main() -> ! {
     rtc.rwdt.disable();
 
     // Turn on-board amber and white LEDs off.
-    peripherals.USB_DEVICE.conf0.modify(|_,w| w.usb_pad_enable().clear_bit());
+    peripherals
+        .USB_DEVICE
+        .conf0
+        .modify(|_, w| w.usb_pad_enable().clear_bit());
 
     let mut leds = (
         io.pins.gpio18.into_push_pull_output(),
-        io.pins.gpio19.into_push_pull_output()
+        io.pins.gpio19.into_push_pull_output(),
     );
 
     leds.0.set_low().unwrap();
@@ -112,7 +120,7 @@ fn main() -> ! {
     let rgb_gpio = (
         io.pins.gpio3.into_push_pull_output(),
         io.pins.gpio4.into_push_pull_output(),
-        io.pins.gpio5.into_push_pull_output()
+        io.pins.gpio5.into_push_pull_output(),
     );
 
     let mut ledc = LEDC::new(
@@ -140,37 +148,36 @@ fn main() -> ! {
         .unwrap();
 
     let mut channel1 = ledc.get_channel(channel::Number::Channel1, rgb_gpio.1);
-        channel1
-            .configure(channel::config::Config {
-                timer: &lstimer0,
-                duty_pct: 10,
-            })
-            .unwrap();
+    channel1
+        .configure(channel::config::Config {
+            timer: &lstimer0,
+            duty_pct: 10,
+        })
+        .unwrap();
 
     let mut channel2 = ledc.get_channel(channel::Number::Channel2, rgb_gpio.2);
-        channel2
-            .configure(channel::config::Config {
-                timer: &lstimer0,
-                duty_pct: 10,
-            })
-            .unwrap();
+    channel2
+        .configure(channel::config::Config {
+            timer: &lstimer0,
+            duty_pct: 10,
+        })
+        .unwrap();
 
     // Update duty cycles. This is done directly by touching the hardware registers. It relies on the side effects of the above setup code.
-    fn set_rgb(r: u32, g: u32, b: u32)
-    {
+    fn set_rgb(r: u32, g: u32, b: u32) {
         let raw_ledc = unsafe { &*hal::pac::LEDC::ptr() };
 
         raw_ledc.ch0_duty.write(|w| w.duty().variant(r));
-        raw_ledc.ch0_conf1.modify(|_,w| w.duty_start().set_bit());  // SC, Self Clear
-        raw_ledc.ch0_conf0.modify(|_,w| w.para_up().set_bit());     // TW, Write Trigger
+        raw_ledc.ch0_conf1.modify(|_, w| w.duty_start().set_bit()); // SC, Self Clear
+        raw_ledc.ch0_conf0.modify(|_, w| w.para_up().set_bit()); // TW, Write Trigger
 
         raw_ledc.ch1_duty.write(|w| w.duty().variant(g));
-        raw_ledc.ch1_conf1.modify(|_,w| w.duty_start().set_bit());  // SC, Self Clear
-        raw_ledc.ch1_conf0.modify(|_,w| w.para_up().set_bit());     // TW, Write Trigger
+        raw_ledc.ch1_conf1.modify(|_, w| w.duty_start().set_bit()); // SC, Self Clear
+        raw_ledc.ch1_conf0.modify(|_, w| w.para_up().set_bit()); // TW, Write Trigger
 
         raw_ledc.ch2_duty.write(|w| w.duty().variant(b));
-        raw_ledc.ch2_conf1.modify(|_,w| w.duty_start().set_bit());  // SC, Self Clear
-        raw_ledc.ch2_conf0.modify(|_,w| w.para_up().set_bit());     // TW, Write Trigger
+        raw_ledc.ch2_conf1.modify(|_, w| w.duty_start().set_bit()); // SC, Self Clear
+        raw_ledc.ch2_conf0.modify(|_, w| w.para_up().set_bit()); // TW, Write Trigger
     }
 
     // Make RGB LED red
@@ -192,8 +199,9 @@ fn main() -> ! {
         rts: UnconnectedPin,
     };
 
-    let mut serial1 = SERIAL.init(peripherals.UART1, config, pins, &clocks).unwrap();
-
+    let mut serial1 = SERIAL
+        .init(peripherals.UART1, config, pins, &clocks)
+        .unwrap();
 
     let mut storage = create_network_stack_storage!(2, 8, 1, 1);
     let ethernet = create_network_interface(network_stack_storage!(storage));
@@ -205,7 +213,7 @@ fn main() -> ! {
     let syst = SystemTimer::new(peripherals.SYSTIMER);
     esp_wifi::initialize(syst.alarm0, peripherals.RNG, &clocks).unwrap();
 
-    while !is_starting(&wifi_interface) { }
+    while !is_starting(&wifi_interface) {}
 
     println!("status = {:?}", wifi_interface.get_status().0);
 
@@ -218,7 +226,7 @@ fn main() -> ! {
     let res = wifi_interface.set_configuration(&client_config);
     println!("set_configuration = {:?}", res);
 
-    while !is_started(&wifi_interface) { }
+    while !is_started(&wifi_interface) {}
 
     println!("status = {:?}", wifi_interface.get_status().0);
 
@@ -248,16 +256,17 @@ fn main() -> ! {
 
     let tcp_handle = wifi_interface.network_interface().add_socket(socket);
 
-
     let mut socket_state = TcpState::Closing;
     let mut uplink_almost_full = false;
     let mut uplink_state = UplinkState::Normal;
 
-
-    hal::interrupt::enable(hal::pac::Interrupt::UART1, hal::interrupt::Priority::Priority3).unwrap();
+    hal::interrupt::enable(
+        hal::pac::Interrupt::UART1,
+        hal::interrupt::Priority::Priority3,
+    )
+    .unwrap();
 
     loop {
-
         wifi_interface.poll_dhcp().unwrap();
 
         wifi_interface
@@ -265,27 +274,31 @@ fn main() -> ! {
             .poll(timestamp())
             .unwrap();
 
-        let tcp_socket = wifi_interface.network_interface().get_socket::<TcpSocket>(tcp_handle);
+        let tcp_socket = wifi_interface
+            .network_interface()
+            .get_socket::<TcpSocket>(tcp_handle);
 
         if !tcp_socket.is_open() {
             tcp_socket.listen(4000).unwrap();
         }
 
         if tcp_socket.can_recv() {
-            tcp_socket.recv(|bytes| {
+            tcp_socket
+                .recv(|bytes| {
+                    // Pump data from TCP buffer into UART HW fifo
+                    let n = bytes
+                        .iter()
+                        .copied()
+                        .map_while(|byte| match serial1.write(byte) {
+                            Ok(_) => Some(()),
+                            Err(_) => None,
+                        })
+                        .count();
 
-                // Pump data from TCP buffer into UART HW fifo
-                let n = bytes.iter().copied().map_while(|byte| {
-                    match serial1.write(byte) {
-                        Ok(_)  => Some(()),
-                        Err(_) => None,
-                    }
-                }).count();
-
-                (n, ())
-            }).unwrap();
+                    (n, ())
+                })
+                .unwrap();
         }
-
 
         // if serial1.rx_fifo_full_interrupt_set() {
         //     uplink_state = UplinkState::Flush;
@@ -293,41 +306,38 @@ fn main() -> ! {
         //     serial1.reset_rx_fifo_full_interrupt()
         // }
 
-
-        if tcp_socket.can_send() && uplink_state != UplinkState::Flush{
-
+        if tcp_socket.can_send() && uplink_state != UplinkState::Flush {
             if uplink_state == UplinkState::Empty {
+                tcp_socket
+                    .send(|bytes| {
+                        if bytes.len() >= 5 {
+                            uplink_state = UplinkState::Normal;
+                            bytes[0..5].copy_from_slice(b"---\r\n");
 
-                tcp_socket.send(|bytes| {
-                    if bytes.len() >= 5 {
-
-                        uplink_state = UplinkState::Normal;
-                        bytes[0..5].copy_from_slice(b"---\r\n");
-
-                        (5, ())
-                    } else {
-                        (0, ())
-                    }
-
-                }).unwrap();
-
-            } else {
-
-                tcp_socket.send(|bytes| {
-
-                    // Fill TCP buffer with data from UART fifo
-                    let n = bytes.iter_mut().map_while(|byte|{
-                        match serial1.read() {
-                            Ok(c) => {
-                                *byte = c;
-                                Some(())
-                            },
-                            Err(_) => None,
+                            (5, ())
+                        } else {
+                            (0, ())
                         }
-                    }).count();
+                    })
+                    .unwrap();
+            } else {
+                tcp_socket
+                    .send(|bytes| {
+                        // Fill TCP buffer with data from UART fifo
+                        let n = bytes
+                            .iter_mut()
+                            .map_while(|byte| match serial1.read() {
+                                Ok(c) => {
+                                    *byte = c;
+                                    Some(())
+                                }
+                                Err(_) => None,
+                            })
+                            .count();
 
-                    (n, ())
-                }).unwrap();
+                        (n, ())
+                    })
+                    .unwrap();
             }
         }
 
@@ -349,7 +359,7 @@ fn main() -> ! {
         }
 
         {
-            let prev= uplink_almost_full;
+            let prev = uplink_almost_full;
             uplink_almost_full = (tcp_socket.send_capacity() - tcp_socket.send_queue()) < 64;
             if uplink_almost_full != prev {
                 println!("uplink_almost_full = {}", uplink_almost_full);
@@ -364,5 +374,4 @@ fn main() -> ! {
             }
         }
     }
-
 }
